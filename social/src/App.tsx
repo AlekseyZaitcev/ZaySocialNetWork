3f7dea5b82a8130e023e9824a5e8a40baf9cf251
AlekseyZaitcev <inquisitorru@mail.ru>
2023-05-07 09:05:01 +0400

    Реализация страницы - 404

--- social/src/App.tsx
+++ social/src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Message from "./pages/Message";
 import Friends from "./pages/Friends";
+import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Route path="/" element={<HomePage />} />
       <Route path="/message" element={<Message />} />
       <Route path="/friends" element={<Friends />} />
+      <Route path="*" element={<NotFoundPage />}></Route>
     </Routes>
   );
 }
